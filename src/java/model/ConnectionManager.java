f6178f8508d503a125c259cbe6dbc5e8bfc982f9
stareginio <sophiecodes02@gmail.com>
2022-02-27 17:23:40 +0800

    Add ConnectionManager, CredentialValidator, User
    added java classes

---
+++ src/java/model/ConnectionManager.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.*;
+
+/**
+ *
+ * @author star
+ */
+public class ConnectionManager {
+    
+    public Connection getConn(String driver, String username, String password,
+                    String driverUrl, String hostname, String port, String database) {
+        
+        Connection conn = null;
+        
+        try {
+            // Load Driver
+            Class.forName(driver);
+        
+            // Establish Connection
+            StringBuffer url = new StringBuffer(driverUrl)
+                        .append("://")
+                        .append(hostname)
+                        .append(":")
+                        .append(port)
+                        .append("/")
+                        .append(database)
+                        .append("?useSSL=false");
+            
+            conn = DriverManager.getConnection(url.toString(), username, password);
+        }
+        
+        catch (SQLException sqle) {
+            System.out.println("SQLException error occured - "
+                                + sqle.getMessage());
+        }
+        
+        catch (ClassNotFoundException nfe) {
+            System.out.println("ClassNotFoundException error occured - "
+                                + nfe.getMessage());
+        }
+        
+        return conn;
+    }
+}
---
+++ src/java/model/CredentialValidator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author star
+ */
+public class CredentialValidator {
+    private User user;
+    private Connection conn;
+    
+    public CredentialValidator(Connection conn) {
+        this.conn = conn;
+    }
+    
+    public boolean checkCreds(String username, String password) {
+        user = new User();
+        
+        try {
+            String queryUsername = "";
+            PreparedStatement ps = ;
+            
+            if ( && )
+            
+        }
+        
+        catch (SQLException sqle) {
+            sqle.printStackTrace(SELECT );
+        }
+        
+        return false;
+    }
+    
+}
