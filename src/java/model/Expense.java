cfdb0bb2568c4986517807b5e9b98f37c310d811
stareginio <sophiecodes02@gmail.com>
2022-03-02 14:28:48 +0800

    Create Expense.java

---
+++ src/java/model/Expense.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.*;
+
+/**
+ *
+ * @author star
+ */
+public class Expense {
+    
+    private Date date;
+    private String inex;  // income/expense
+    private double amount;
+    private String category;
+    private String note;
+    
+    public Expense() { }
+    
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the inex
+     */
+    public String getInex() {
+        return inex;
+    }
+
+    /**
+     * @param ie the inex to set
+     */
+    public void setInex(String ie) {
+        this.inex = ie;
+    }
+
+    /**
+     * @return the amount
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(String s) {
+        Double amount = Double.parseDouble(s);
+        
+        this.amount = amount;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the note
+     */
+    public String getNote() {
+        return note;
+    }
+
+    /**
+     * @param note the note to set
+     */
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
