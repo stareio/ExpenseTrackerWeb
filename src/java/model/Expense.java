ec60bee4a6108e1f35bd42d0c0662447f7ba953c
stareginio <sophiecodes02@gmail.com>
2022-03-05 21:22:48 +0800

    Update Expense.java
    removed the comments of the getters & setters

--- src/java/model/Expense.java
+++ src/java/model/Expense.java
@@ -52,74 +52,44 @@ public class Expense {
         return strDate;
     }
     
-    /**
-     * @return the date
-     */
     public Date getDate() {
         return date;
     }
 
-    /**
-     * @param date the date to set
-     */
     public void setDate(Date date) {
         this.date = date;
     }
 
-    /**
-     * @return the inex
-     */
     public String getInex() {
         return inex;
     }
 
-    /**
-     * @param ie the inex to set
-     */
     public void setInex(String ie) {
         this.inex = ie;
     }
 
-    /**
-     * @return the amount
-     */
     public double getAmount() {
         return amount;
     }
 
-    /**
-     * @param amount the amount to set
-     */
     public void setAmount(String s) {
         Double amount = Double.parseDouble(s);
         
         this.amount = amount;
     }
 
-    /**
-     * @return the category
-     */
     public String getCategory() {
         return category;
     }
 
-    /**
-     * @param category the category to set
-     */
     public void setCategory(String category) {
         this.category = category;
     }
 
-    /**
-     * @return the descr
-     */
     public String getDescription() {
         return descr;
     }
 
-    /**
-     * @param descr the descr to set
-     */
     public void setDescription(String descr) {
         this.descr = descr;
     }
