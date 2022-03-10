11b7914a7b239220cf2826a49b8451f04cfef117
stareginio <sophiecodes02@gmail.com>
2022-03-10 19:51:03 +0800

    Update EntryValidator.java
    updated the checkDate method. added the checkAmount method.

--- src/java/model/EntryValidator.java
+++ src/java/model/EntryValidator.java
@@ -5,6 +5,8 @@
  */
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.util.*;
 import javax.swing.JOptionPane;
@@ -14,22 +16,48 @@ import javax.swing.JOptionPane;
  * @author star
  */
 public class EntryValidator {
+    // methods return true if input is valid
     
-    // returns true if input date is valid
-    public boolean checkDate(Date date) {
+    public boolean checkDate(String str) {
         
         try {
-            // throws DTE if an invalid date value is passed to it
-            LocalDate ld = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            // check the length
+            if (str.length() == 10){
+                
+                // reformat the input date
+                SimpleDateFormat sdfParse = new SimpleDateFormat("MM/dd/yyyy");
+                SimpleDateFormat sdfFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date parsedDate = sdfParse.parse(str);
 
-            return true;
+                // throws a DTE if an invalid date value is passed to it
+                LocalDate ld = parsedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+                return true;
+            }
+        }
+        
+        catch (ParseException pe) {
+            pe.printStackTrace();
         }
         
         catch (DateTimeException dte) {
-            JOptionPane.showMessageDialog(null, "Please enter a valid date", "Error Screen",
-                    JOptionPane.ERROR_MESSAGE);
-        }
+            dte.printStackTrace();
+        } 
 
+        return false;
+    }
+    
+    public boolean checkAmount(String amount) {
+        try {
+            double d = Double.valueOf(amount);
+            
+            return true;
+        }
+        
+        catch (NumberFormatException nfe) {
+            nfe.printStackTrace();
+        }
+        
         return false;
     }
 }
